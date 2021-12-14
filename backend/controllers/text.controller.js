054620fcbccd9191dd582d760b8e131496fb2843
Luis Veliz <ti4.inqube@gmail.com>
2021-12-13 23:44:21 -0500

    first commit

---
+++ backend/controllers/text.controller.js
@@ -0,0 +1,25 @@
+const textController = {};
+
+textController.getInvertedText = (req, res) => {
+  if (req.query.text) {
+    //option 1
+    let inverted = req.query.text.split("").reverse().join("");
+    let palindrome = false;
+    if (inverted === req.query.text) {
+      palindrome = true;
+    }
+    //option 2
+    // let inverted = "";
+    // for (let index = req.query.text.length - 1; index >= 0; index--) {
+    //   inverted += req.query.text[index];
+    // }
+
+    res.setHeader("Content-Type", "application/json");
+
+    return res.status(200).send({ text: inverted, palindrome: palindrome });
+  } else {
+    return res.status(400).send({ error: "no text" });
+  }
+};
+
+module.exports = textController;
---
+++ backend/routes/text.routes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const textController = require("../controllers/text.controller");
+router.get("/iecho", textController.getInvertedText);
+
+module.exports = router;
