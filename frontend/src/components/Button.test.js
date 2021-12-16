c718b16e16097893a5db532aa5bb4668792b50c8
Luis Veliz <ti4.inqube@gmail.com>
2021-12-16 04:16:31 -0500

    feat: adding tests

---
+++ frontend/src/components/Button.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Button from "./Button";
+import jest from "jest";
+import "@testing-library/jest-dom";
+
+test("Render Button with text", () => {
+  const text = "boton";
+
+  const component = render(<Button text={text} />);
+  component.getByText("boton");
+  component.debug();
+});
+
+test("Render Button with text and onClick", () => {
+  const text = "boton";
+  const onClick = jest.mock();
+  const component = render(<Button text={text} onClick={onClick} />);
+  component.getByText("boton");
+});
